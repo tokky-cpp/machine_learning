468ec901acea384f176e721d9dfa1f401d4ded83
tokky <tom94826@gmail.com>
2016-07-20 16:49:03 +0900

    constant

--- gmm.py
+++ gmm.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+POINT = 100
+
 mu = [[-10,10],[10,10],[10,-10],[-10,-10]]
-sigma = [[1,1],[1,1],[1,1],[1,1]]
+sigma = [[5,5],[5,5],[5,5],[5,5]]
 
 vec = []
 
 for i in range(4):
     x = []
     y = []
-    for j in range(5):
+    for j in range(POINT):
         x1 = np.random.normal(mu[i][0],sigma[i][0])
         x.append(x1)
 
